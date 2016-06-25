eb8f43283645592bafc58eff69c071db7f8f0357
Ott Tooming <OttTooming@gmail.com>
2016-06-25 17:03:12 +0300

    Update WC empty cart template

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -4,21 +4,26 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.0.0
+ * @version   2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 wc_print_notices();
-
 ?>
 
 <?php do_action('woocommerce_cart_is_empty'); ?>
 
 <div class="empty-cart-block">
-	<i class="icon-shopping-cart"></i>
-	
-	<?php etheme_option('empty_cart_content'); ?>	
-	<p><a class="button arrow-left" href="<?php echo get_permalink(woocommerce_get_page_id('shop')); ?>"><span><?php _e('Return To Shop', ETHEME_DOMAIN) ?></span></a></p>
-	
+		<i class="icon-shopping-cart"></i>
+
+		<?php etheme_option('empty_cart_content'); ?>
+		<p>
+				<a class="button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+						<?php _e( 'Return To Shop', 'woocommerce' ) ?>
+				</a>
+		</p>
+
 </div>
