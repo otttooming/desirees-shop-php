794faee4b7f128cffc04225d975ada8a5a6f46f1
Ott Tooming <OttTooming@gmail.com>
2016-07-06 17:15:35 +0300

    Update WC short description classes

--- woocommerce/single-product/short-description.php
+++ woocommerce/single-product/short-description.php
@@ -4,17 +4,24 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     1.6.4
- */
+ * @version   1.6.4
+*/
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-global $post;
-
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 ?>
 
-<?php echo content(40); ?>
+<div class="product__excerpt">
+  <?php echo content(40); ?>
+</div>
 
-<a href="#content_tab_1" class="more-info"><?php _e('More information', ETHEME_DOMAIN) ?></a>
+<a class="more-info" href="#content_tab_1" >
+  <span class="more-info__text">
+    <?php _e('More information', ETHEME_DOMAIN) ?>
+  </span>
+	
+  <i class="more-info__icon icon-chevron-down"></i>
+</a>
 
-<hr />
+<hr>
