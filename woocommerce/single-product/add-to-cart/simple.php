98fabd9c8ce15eb2d2d934faa032a7197afbad51
Ott Tooming <OttTooming@gmail.com>
2016-07-24 19:44:58 +0300

    Update WC add-to-cart templates

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -7,41 +7,46 @@
  * @version     2.1.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-global $woocommerce, $product;
+global $product;
 
-if ( ! $product->is_purchasable() ) return;
-$ajax_addtocart = etheme_get_option('ajax_addtocart');
+if ( ! $product->is_purchasable() ) {
+	return;
+}
 ?>
 
 
-<div class="addto-container">
 <?php if ( $product->is_in_stock() ) : ?>
+	<div class="addto-container cfx">
 
-	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-	<form class="cart cfx" method="post" enctype='multipart/form-data'>
-	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+		<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-	 	<?php
-	 		if ( ! $product->is_sold_individually() )
-	 			woocommerce_quantity_input( array(
-	 				'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-	 				'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
-	 			) );
-	 	?>
+		<form class="cart cfx" method="post" enctype='multipart/form-data'>
+			<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+			<?php
+				if ( ! $product->is_sold_individually() )
+					woocommerce_quantity_input( array(
+						'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
+						'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product ),
+						// 'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) // Adds back value from customer selection
+						) );
+			?>
 
-	 	<button type="submit" class="button big active <?php if($ajax_addtocart){?> etheme-simple-product <?php } ?>"><span><?php echo $product->single_add_to_cart_text(); ?></span></button>
+			<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
-	</form>
+			<button type="submit" class="button big active"><span><?php echo $product->single_add_to_cart_text(); ?></span></button>
 
-	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+			<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+		</form>
 
+		<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+
+	</div>
+
+	<hr>
 <?php endif; ?>
-</div>
-<div class="clear"></div>
-<hr/>
