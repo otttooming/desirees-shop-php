82294960be69016e6541f0fa2c6f896cef3c1542
Ott Tooming <OttTooming@gmail.com>
2016-08-08 07:58:52 +0300

    Fix link inline-block visual inconsistencies

--- inc/func/dequeue.php
+++ inc/func/dequeue.php
@@ -83,3 +83,6 @@ function disable_x_pingback( $headers ) {
 
 return $headers;
 }
+
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
