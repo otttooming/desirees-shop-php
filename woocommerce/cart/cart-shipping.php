0999dc9c8aaabcb5c75bf0a3a6ad272f26474e30
Ott Tooming <OttTooming@gmail.com>
2016-08-20 16:59:46 +0300

    Add product links to review order view

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <tr class="shipping">
 	<th colspan="2"><?php echo wp_kses_post( $package_name ); ?></th>
-	<td data-title="<?php echo esc_attr( $package_name ); ?>">
+	<td colspan="2" data-title="<?php echo esc_attr( $package_name ); ?>">
 		<?php if ( 1 < count( $available_methods ) ) : ?>
 			<ul id="shipping_method">
 				<?php foreach ( $available_methods as $method ) : ?>
--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -10,12 +10,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+global $woocommerce;
+
 ?>
 <table class="shop_table checkout_cart table woocommerce-checkout-review-order-table">
 	<thead class="cart__form-head">
 		<tr>
       <th class="product-thumbnail cart_del_column">&nbsp;</th>
 			<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+      <th class="product-quantity"><?php _e('Qty', ETHEME_DOMAIN); ?></th>
 			<th class="product-total"><?php _e( 'Total', 'woocommerce' ); ?></th>
 		</tr>
 	</thead>
@@ -37,10 +40,36 @@ if ( ! defined( 'ABSPATH' ) ) {
   						?>
   					</td>
             <td class="product-name">
-							<?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ); ?>
-							<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times; %s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); ?>
-							<?php echo WC()->cart->get_item_data( $cart_item ); ?>
-						</td>
+  						<?php
+  							if ( ! $_product->is_visible() || ( $_product instanceof WC_Product_Variation && ! $_product->parent_is_visible() ) )
+  								echo apply_filters( 'woocommerce_in_cart_product_title', $_product->get_title(), $cart_item, $cart_item_key );
+  							else
+  								printf('<a href="%s">%s</a>', esc_url( get_permalink( apply_filters('woocommerce_in_cart_product_id', $cart_item['product_id'] ) ) ), apply_filters('woocommerce_in_cart_product_title', $_product->get_title(), $cart_item, $cart_item_key ) );
+
+  							// Meta data
+  							echo $woocommerce->cart->get_item_data( $cart_item );
+
+                 				// Backorder notification
+                 				if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) )
+                 					echo '<p class="backorder_notification">' . __('Available on backorder', ETHEME_DOMAIN) . '</p>';
+  						?>
+  					</td>
+            <!-- Quantity inputs -->
+            <td class="product-quantity" id="cart-quantity">
+              <?php
+                if ( $_product->is_sold_individually() ) {
+                  $product_quantity = '1';
+                } else {
+                  $data_min = apply_filters( 'woocommerce_cart_item_data_min', '', $_product );
+                  $data_max = ( $_product->backorders_allowed() ) ? '' : $_product->get_stock_quantity();
+                  $data_max = apply_filters( 'woocommerce_cart_item_data_max', $data_max, $_product );
+
+                  $product_quantity = sprintf( '<div class="qty-block quantity"><input name="cart[%s][qty]" type="number" data-min="%s" data-max="%s" value="%s" size="4" title="Qty" class="input-text qty text" maxlength="12" readonly /></div>', $cart_item_key, $data_min, $data_max, esc_attr( $cart_item['quantity'] ) );
+                }
+
+                echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key );
+              ?>
+            </td>
 						<td class="product-total">
 							<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); ?>
 						</td>
@@ -56,7 +85,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<tr class="cart-subtotal">
 			<th colspan="2"><?php _e( 'Cart Subtotal', 'woocommerce' ); ?></th>
-			<td><?php wc_cart_totals_subtotal_html(); ?></td>
+			<td colspan="2"><?php wc_cart_totals_subtotal_html(); ?></td>
 		</tr>
 
 		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
@@ -103,7 +132,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<tr class="order-total">
 			<th colspan="2"><?php _e( 'Order Total', 'woocommerce' ); ?></th>
-			<td><?php wc_cart_totals_order_total_html(); ?></td>
+			<td colspan="2"><?php wc_cart_totals_order_total_html(); ?></td>
 		</tr>
 
 		<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
