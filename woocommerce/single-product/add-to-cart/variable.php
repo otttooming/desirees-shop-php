1d5e52122dd22f634c7853a81ae8b3402edc766b
Ott Tooming <OttTooming@gmail.com>
2016-08-24 22:56:06 +0300

    Fix attribute name in product variation

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -35,7 +35,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 			<?php foreach ( $attributes as $attribute_name => $options ) : ?>
         <?php
           $selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( urldecode( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ) : $product->get_variation_default_attribute( $attribute_name );
-          wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected, 'show_option_none' => 'Select'. ' ' . sanitize_title( $attribute_name ) ) );
+          wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected, 'show_option_none' => 'Select'. ' ' . wc_attribute_label( $attribute_name ) ) );
         ?>
 			<?php endforeach; ?>
 		</div>
