381987a76b30efd0b2214be6cc0ef5dda4011694
Ott Tooming <OttTooming@gmail.com>
2016-09-25 19:41:56 +0300

    Remove legacy clictoggle jQuery function

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -11393,20 +11393,6 @@ function listSwitcher() {
   }
 }
 
-$.fn.clicktoggle = function(a, b) {
-    return this.each(function() {
-        var clicked = false;
-        $(this).click(function() {
-            if (clicked) {
-                clicked = false;
-                return b.apply(this, arguments);
-            }
-            clicked = true;
-            return a.apply(this, arguments);
-        });
-    });
-};
-
 $(document).ready(function() {
 	listSwitcher();
 	check_view_mod();
--- src/scripts/legacy/script.js
+++ src/scripts/legacy/script.js
@@ -202,17 +202,3 @@ function listSwitcher() {
     });
   }
 }
-
-$.fn.clicktoggle = function(a, b) {
-    return this.each(function() {
-        var clicked = false;
-        $(this).click(function() {
-            if (clicked) {
-                clicked = false;
-                return b.apply(this, arguments);
-            }
-            clicked = true;
-            return a.apply(this, arguments);
-        });
-    });
-};
