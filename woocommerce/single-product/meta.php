a429f17d59d93ba1e33498dc291a222423926558
Ott Tooming <OttTooming@gmail.com>
2016-10-26 23:51:06 +0300

    Update product meta section styling

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -22,9 +22,9 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php do_action( 'woocommerce_product_meta_start' ); ?>
 
-	<?php echo $product->get_categories( '<span class="separator__tag">&bull;</span>', '<div class="posted_in">' . '<span class="posted-in__header">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . '</span>' . ' ', '</div>' ); ?>
+	<?php echo $product->get_categories( '', '<div class="product__details-block posted_in">' . '<h2 class="posted-in__header">' . _n( 'Category', 'Categories', $cat_count, 'woocommerce' ) . '</h2>' . ' ', '</div>' ); ?>
 
-	<?php echo $product->get_tags( '<span class="separator__tag">&bull;</span>', '<div class="tagged_as">' . '<span class="tagged-as__header">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . '</span>' . ' ', '</div>' ); ?>
+	<?php echo $product->get_tags( '', '<div class="product__details-block tagged_as">' . '<h2 class="tagged-as__header">' . _n( 'Tag', 'Tags', $tag_count, 'woocommerce' ) . '</h2>' . ' ', '</div>' ); ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
