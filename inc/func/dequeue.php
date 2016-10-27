6e0cf2c7bd966741a7c91a664f11d942822f852c
Ott Tooming <OttTooming@gmail.com>
2016-10-27 23:25:51 +0300

    Remove inline WC jQuery

--- inc/func/dequeue.php
+++ inc/func/dequeue.php
@@ -131,3 +131,44 @@ function desirees_override_shipping_fields( $fields ) {
 //   unset($fields['shipping_city']);
   return $fields;
 }
+
+/**
+ * Manage WooCommerce styles and scripts.
+ */
+function grd_woocommerce_script_cleaner() {
+
+	// Remove the generator tag
+	remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+	// Unless we're in the store, remove all the cruft!
+	if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+		wp_dequeue_style( 'woocommerce_frontend_styles' );
+		wp_dequeue_style( 'woocommerce-general');
+		wp_dequeue_style( 'woocommerce-layout' );
+		wp_dequeue_style( 'woocommerce-smallscreen' );
+		wp_dequeue_style( 'woocommerce_fancybox_styles' );
+		wp_dequeue_style( 'woocommerce_chosen_styles' );
+		wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+		wp_dequeue_style( 'select2' );
+		wp_dequeue_script( 'wc-add-payment-method' );
+		wp_dequeue_script( 'wc-lost-password' );
+		wp_dequeue_script( 'wc_price_slider' );
+		wp_dequeue_script( 'wc-single-product' );
+		wp_dequeue_script( 'wc-add-to-cart' );
+		wp_dequeue_script( 'wc-cart-fragments' );
+		wp_dequeue_script( 'wc-credit-card-form' );
+		wp_dequeue_script( 'wc-checkout' );
+		wp_dequeue_script( 'wc-add-to-cart-variation' );
+		wp_dequeue_script( 'wc-single-product' );
+		wp_dequeue_script( 'wc-cart' );
+		wp_dequeue_script( 'wc-chosen' );
+		wp_dequeue_script( 'woocommerce' );
+		wp_dequeue_script( 'prettyPhoto' );
+		wp_dequeue_script( 'prettyPhoto-init' );
+		wp_dequeue_script( 'jquery-blockui' );
+		wp_dequeue_script( 'jquery-placeholder' );
+		wp_dequeue_script( 'jquery-payment' );
+		wp_dequeue_script( 'fancybox' );
+		wp_dequeue_script( 'jqueryui' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'grd_woocommerce_script_cleaner', 99 );
