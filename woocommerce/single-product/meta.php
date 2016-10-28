ca1e0e4863323cb44032e298e9947e32e8ea47b8
Ott Tooming <OttTooming@gmail.com>
2016-10-28 21:39:46 +0300

    Fix product meta section last item margin

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -18,14 +18,10 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 ?>
 
-<div class="product__meta">
+<?php do_action( 'woocommerce_product_meta_start' ); ?>
 
-	<?php do_action( 'woocommerce_product_meta_start' ); ?>
+<?php echo $product->get_categories( '', '<div class="product__details-block product-meta posted_in">' . '<h2 class="posted-in__header">' . _n( 'Category', 'Categories', $cat_count, 'woocommerce' ) . '</h2>' . ' ', '</div>' ); ?>
 
-	<?php echo $product->get_categories( '', '<div class="product__details-block posted_in">' . '<h2 class="posted-in__header">' . _n( 'Category', 'Categories', $cat_count, 'woocommerce' ) . '</h2>' . ' ', '</div>' ); ?>
+<?php echo $product->get_tags( '', '<div class="product__details-block product-meta tagged_as">' . '<h2 class="tagged-as__header">' . _n( 'Tag', 'Tags', $tag_count, 'woocommerce' ) . '</h2>' . ' ', '</div>' ); ?>
 
-	<?php echo $product->get_tags( '', '<div class="product__details-block tagged_as">' . '<h2 class="tagged-as__header">' . _n( 'Tag', 'Tags', $tag_count, 'woocommerce' ) . '</h2>' . ' ', '</div>' ); ?>
-
-	<?php do_action( 'woocommerce_product_meta_end' ); ?>
-
-</div>
+<?php do_action( 'woocommerce_product_meta_end' ); ?>
