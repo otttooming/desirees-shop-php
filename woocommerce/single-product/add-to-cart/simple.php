9a44e6b32c8dbd210bfdb2328a40ffd18e747686
Ott Tooming <OttTooming@gmail.com>
2016-10-30 09:02:51 +0200

    Add cart+ icon to product buy btn

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -25,7 +25,7 @@ if ( ! $product->is_purchasable() ) {
 
 		<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-		<form class="cart__add-to" method="post" enctype='multipart/form-data'>
+		<form class="addto" method="post" enctype='multipart/form-data'>
 			<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 			<?php
@@ -33,13 +33,17 @@ if ( ! $product->is_purchasable() ) {
 					woocommerce_quantity_input( array(
 						'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
 						'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product ),
-						// 'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) // Adds back value from customer selection
+						'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) // Adds back value from customer selection
 						) );
 			?>
 
 			<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
 
-			<button type="submit" class="button big active"><span><?php echo $product->single_add_to_cart_text(); ?></span></button>
+			<label class="button big active">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="addto__icon"><path d="M11 9h2V6h3V4h-3V1h-2v3H8v2h3v3zm-4 9c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2zm-9.83-3.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.86-7.01L19.42 4h-.01l-1.1 2-2.76 5H8.53l-.13-.27L6.16 6l-.95-2-.94-2H1v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.13 0-.25-.11-.25-.25z" fill="currentColor"/></svg>
+				<input type="submit" name="" value="">
+				<?php echo $product->single_add_to_cart_text(); ?>
+			</label>
 
 			<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 		</form>
