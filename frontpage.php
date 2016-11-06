8bb528caa88b1b617fcc1d5a401f01860d7bdb7f
Ott Tooming <OttTooming@gmail.com>
2016-11-06 23:14:41 +0200

    Update FP layout prod cat loop

--- frontpage.php
+++ frontpage.php
@@ -11,14 +11,18 @@
   <?php if (  is_active_sidebar( 'fp-shopnav-1' ) || is_active_sidebar( 'fp-shopnav-2' ) ) : ?>
     <div class="row">
         <?php if (  is_active_sidebar( 'fp-shopnav-1' ) ) : ?>
-          <div class="col-sm-6">
+          <div class="col-xs-12 col-sm-offset-2 col-sm-4">
+            <div class="m-20-0-20-0">
               <?php dynamic_sidebar( 'fp-shopnav-1' ); ?>
+            </div>
           </div>
         <?php endif; ?>
 
         <?php if (  is_active_sidebar( 'fp-shopnav-2' ) ) : ?>
-          <div class="col-sm-6">
-            <?php dynamic_sidebar( 'fp-shopnav-2' ); ?>
+          <div class="col-xs-12 col-sm-4">
+            <div class="m-20-0-20-0">
+              <?php dynamic_sidebar( 'fp-shopnav-2' ); ?>
+            </div>
           </div>
         <?php endif; ?>
     </div>
@@ -95,7 +99,6 @@
 
   <div class="row">
     <?php
-
     $args = array( 'taxonomy' => 'product_cat' );
     $terms = get_terms('product_cat', $args);
 
@@ -103,17 +106,19 @@
         foreach ($terms as $term) {
             if ($term->parent == 0) {
                 $thumbnail_id = get_woocommerce_term_meta( $term->term_id, 'thumbnail_id', true );
+                ?>
 
-                echo '<div class="span3">';
-                  echo '<p class="w100"><a href="' . get_term_link( $term->slug, 'product_cat' ) . '" title="' . sprintf(__('View all products filed under %s', 'desirees'), $term->name) . '" class="w100">' . '<!--<img class="lazyload" data-src="' . $image = wp_get_attachment_url( $thumbnail_id ) . '">-->' . '<span class="button big w100">' . $term->name . '</span>' . '</a></p>';
-                echo '</div>';
+                    <div class="col-sm-3">
+                        <a href="<?php echo get_term_link( $term->slug, 'product_cat' ); ?>" title="<?php echo sprintf(__('View all products filed under %s', 'desirees'), $term->name); ?>" class="">
+                            <img class="lazyload" data-src="<?php echo wp_get_attachment_url( $thumbnail_id ); ?>">
+                            <span><?php echo $term->name; ?></span>
+                        </a>
+                    </div>
+
+                <?php
             }
         }
-    }
-
-    // print_r($terms)
-
-     ?>
+    } ?>
   </div>
 </div>
 
