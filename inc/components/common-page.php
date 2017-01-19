c4389809bdb6c975632659281a7193e34acfc1f8
Ott Tooming <OttTooming@gmail.com>
2017-01-19 14:31:52 +0200

    Fix bg__common on common-page for WC shortcodes

--- inc/components/common-page.php
+++ inc/components/common-page.php
@@ -14,18 +14,28 @@
           <?php get_sidebar(); ?>
         <?php endif; ?>
 
-        <div class="<?php echo ($template_common_page == 'sidebar') ? 'col-md-9' : 'col-xs-12'; ?> grid_content with-sidebar-top cfx">
+        <div class="<?php echo ($template_common_page == 'sidebar') ? 'col-xs-12 col-lg-9' : 'col-xs-12'; ?> grid_content with-sidebar-top cfx">
           <?php
               $post_id = $wp_query->get_queried_object_id();
               $title = get_post_field('post_title', $post_id);
           ?>
 
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-              <div class="bg__common p1">
+
+              <?php
+              /**
+               * Don't show background and padding for WC shortcodes
+               * Applicable items have independent bg sections
+               *
+               */
+              ?>
+              <div class="<?php echo !has_shortcode($post->post_content, 'woocommerce_cart' ) && !has_shortcode($post->post_content, 'woocommerce_checkout' ) ? 'content__common bg__common bg__common--light p1' : ''; ?>">
                   <?php the_content(); ?>
               </div>
+
               <?php wp_link_pages(array('before' => ''.__('Pages:', 'desirees'), 'after' => '')); ?>
           <?php endwhile; ?>
+
           <?php else : ?>
             <p><strong><?php _e('Not Found', 'desirees'); ?></strong></p>
             <p>
