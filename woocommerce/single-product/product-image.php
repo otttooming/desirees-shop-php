68e169cf5447e271a78cefb2aa4dbe8d8c7529a9
Ott Tooming <OttTooming@gmail.com>
2017-02-07 15:43:51 +0200

    Add responsive srcset for product image

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -11,38 +11,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-global $post, $woocommerce;
-
-$img_padding_ratio = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium_large' )[2] / wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium_large' )[1] * 100 . '%';
-?>
+global $post, $woocommerce; ?>
 
 <div class="col-xs-12 col-md-5 product__left-wrap">
 
-<div class=" product_image product__left">
-	<div class="product__left-inner">
-		<div class="main-image product__main-image-wrap img-padding-ratio__wrap">
-				<?php // etheme_wc_product_labels(); ?>
-				<figure>
-					<a class="main-image product__main-image lightbox" itemprop="image" href="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>" rel="thumbnails" title="<?php echo get_the_title( get_post_thumbnail_id() ); ?>" data-size="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[1] . 'x' . wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[2] ?>">
-							<img
-							class="attachment-shop_single lazyload"
-							data-src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium_large' )[0]; ?>"
-							width="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium_large' )[1] ?>"
-							height="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium_large' )[2] ?>"
-							alt="<?php the_title(); ?>"
-							data-type="<?php echo gettype(wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium_large' )[2]) ?>"
-							>
-
-					</a>
-				</figure>
-
-				<div style="padding-bottom:<?php echo $img_padding_ratio; ?>"></div>
-
-		</div>
+	<div class="product_image product__left">
+		<figure class="main-image product__main-image-wrap">
+			<a class="main-image product__main-image lightbox" itemprop="image" href="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>" rel="thumbnails" title="<?php echo get_the_title( get_post_thumbnail_id() ); ?>" data-size="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[1] . 'x' . wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[2] ?>">
+					<?php echo desirees_get_attachment_image( get_post_thumbnail_id(), 'medium_large', false, ["class" => "lazyload", "itemprop" => "image", "alt" => get_the_title()] ); ?>
+			</a>
+		</figure>
 
 		<?php do_action('woocommerce_product_thumbnails'); ?>
 	</div>
-</div>
-
 
 </div>
