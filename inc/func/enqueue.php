2b4676e4174493e544bd0a8ca17aef1e88dc6825
Ott Tooming <OttTooming@gmail.com>
2017-04-01 16:36:20 +0300

    Add padding and visual update for blockqoute

--- inc/func/enqueue.php
+++ inc/func/enqueue.php
@@ -5,9 +5,9 @@
  */
 function desirees_assets() {
 
-	wp_enqueue_style( 'desirees-style', get_stylesheet_directory_uri() . '/dist/styles/style.min.css', array(), '1', 'all' );
+	wp_enqueue_style( 'desirees-style', get_stylesheet_directory_uri() . '/dist/styles/style.min.css', array(), '2', 'all' );
 
-	wp_enqueue_script( 'desirees-script', get_template_directory_uri() . '/dist/scripts/script.min.js', array(), '1', true );
+	wp_enqueue_script( 'desirees-script', get_template_directory_uri() . '/dist/scripts/script.min.js', array(), '2', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'desirees_assets' );
