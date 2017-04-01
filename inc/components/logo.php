2cb2309f2e4d1b1346f83fe4dc4ff02717220d5f
Ott Tooming <OttTooming@gmail.com>
2017-04-01 21:55:29 +0300

    Fix logo resposive img srcset

--- inc/components/logo.php
+++ inc/components/logo.php
@@ -8,21 +8,10 @@
  */
 
  $custom_logo_id = get_theme_mod( 'custom_logo' );
- $logoimg = wp_get_attachment_image_src( $custom_logo_id , 'full' );
- if ($logoimg) {
-   $img_padding_ratio = $logoimg[2] / $logoimg[1] * 100 . '%';
- }
  ?>
 
-<?php if ($logoimg) : ?>
-    <div class="img-padding-ratio__wrap">
-
-        <a href="<?php echo home_url(); ?>">
-            <img src="<?php echo $logoimg[0]; ?>" width="<?php echo $logoimg[1]; ?>" height="<?php echo $logoimg[2]; ?>" alt="<?php bloginfo( 'description' ); ?>" class="header__logo-img" />
-        </a>
-
-        <div style="padding-bottom:<?php echo $img_padding_ratio; ?>"></div>
-    </div>
+<?php if ($custom_logo_id) : ?>
+    <?php echo desirees_get_attachment_image(  $custom_logo_id, 'medium_large', false, ["class" => "lazyload", "itemprop" => "image", "alt" => get_bloginfo('name')] ); ?>
 <?php else : ?>
-  <a href="<?php echo home_url(); ?>"><span class="logo-text-red">DE</span>SIREE</a>
+  <a href="<?php echo home_url(); ?>"><?php echo get_bloginfo('name'); ?></a>
 <?php endif; ?>
