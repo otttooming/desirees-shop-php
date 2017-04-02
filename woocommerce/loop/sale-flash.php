4a08eae787af52c3b92d6fd8b7d1e38bb1075c99
Ott Tooming <OttTooming@gmail.com>
2017-04-02 15:20:55 +0300

    Update product sale visuals

--- woocommerce/loop/sale-flash.php
+++ woocommerce/loop/sale-flash.php
@@ -25,7 +25,7 @@ global $post, $product;
 ?>
 <?php if ( $product->is_on_sale() ) : ?>
 
-	<?php echo apply_filters( 'woocommerce_sale_flash', '<div class="product-sale"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.5 29.5" width="15" height="15" fill="currentColor" class="product-sale__label"><path d="M15.205.043L1.06 15.203 0 16.262l12.793 11.81 1.56 1.41 14.67-16.032L29.5.017 15.205.043zm6.822 9.385a2.647 2.647 0 1 1 .002-5.294 2.647 2.647 0 0 1-.003 5.294z"/></svg><span>' . __( 'Sale!', 'woocommerce' ) . '</span></div>', $post, $product ); ?>
+	<?php echo apply_filters( 'woocommerce_sale_flash', '<div class="sale"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.5 29.5" width="16" height="16" fill="currentColor" class="sale__label"><path d="M15.205.043L1.06 15.203 0 16.262l12.793 11.81 1.56 1.41 14.67-16.032L29.5.017 15.205.043zm6.822 9.385a2.647 2.647 0 1 1 .002-5.294 2.647 2.647 0 0 1-.003 5.294z"/></svg>' . __( 'Sale!', 'woocommerce' ) . '</div>', $post, $product ); ?>
 
 <?php endif;
 
